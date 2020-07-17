ff9a040758b229f7cd410d7c9bd4b95600bdc18f
wtang8 <wai_shing_tang@brown.edu>
2020-07-17 14:50:43 -0400

    7/17/2020

--- test_script/main_gpclassification.py
+++ test_script/main_gpclassification.py
@@ -10,37 +10,45 @@ from gp import *
 import numpy as np
 import os
 
+X = []
+y = []
+
+prefixes = ['ec_sphere/WT/WT_chimera_','ec_sphere/R164S/R164S_chimera_']#,'ec_sphere/R164G/R164G_chimera_']
+n_sample = 20
+class_labels = [0,1]#,2]
+
+for i in range(len(prefixes)):
+    for j in range(1,n_sample+1):
+        ec = np.loadtxt('%s%d.dat'%(prefixes[i],j))[1:].flatten()
+        if ec[-1] > 100 and ec[-1] < 200:
+            X.append(ec)
+            y.append(class_labels[i])
+X = np.array(X)
+y = np.array(y)
+
+# Hard coded Laplace Approximation
+K = GuassianKernel(X,bandwidth=0.1)
+mean, sigma = ExpectationPropagation(K,y)
+print(mean,len(mean))
+print(sigma,sigma.shape)
+
+
+'''
+import pymc3 as pm
+latent_gp_model = pm.Model()
+
+conv_func = pm.gp.cov.ExpQuad(1, ls=0.1)
+gp = pm.gp.Latent(conv_func=conv_func)
+f = gp.prior("f",X=X)
+'''
+
+
+'''
+## sk learn GPC
 
 from sklearn.gaussian_process import GaussianProcessClassifier
 from sklearn.gaussian_process.kernels import RBF
 
-X = []
-y = []
-for i in range(1,91):
-    ec = np.loadtxt('ec_sphere/WT/WT_chimera_%d.dat'%i)[1:].flatten()
-    if ec[-1] > 100 and ec[-1] < 200:
-        X.append(ec)
-        y.append(0)
-for i in range(1,91):
-    ec = np.loadtxt('ec_sphere/R164S/R164S_chimera_%d.dat'%i)[1:].flatten()
-    if ec[-1] > 100 and ec[-1] < 200:
-        X.append(ec)
-        y.append(1)
-#for i in range(1,91):
-#    ec = np.loadtxt('ec_sphere_2/R164G/R164G_chimera_%d.dat'%i)[1:].flatten()
-#    if ec[-1] > 100 and ec[-1] < 200:
-#        X.append(ec)
-#        y.append(2)
-
-X = np.array(X)
-y = np.array(y)
-
-Kn = GuassianKernel(X,bandwidth=0.02)
-mean,covariance = LaplaceApproximation(Kn,y)
-#print(mean,len(mean))
-#print(covariance,covariance.shape)
-
-'''
 kernel = 1.0*RBF(1.0)
 gpc = GaussianProcessClassifier(kernel=kernel,random_state=0,max_iter_predict=1000).fit(X,y)
 
@@ -55,8 +63,5 @@ X = np.array(X)
 print(gpc.predict_proba(X[:,:]))
 print(gpc.predict(X[:,:]))
 '''
-
-
-
 
 
