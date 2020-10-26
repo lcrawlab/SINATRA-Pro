9901a4ed621a78e711d96be3783f31250ea7748f
wtang8 <wai_shing_tang@brown.edu>
2020-10-25 22:59:15 -0400

    10/25/2020 Wai Shing

--- test_script/mesh.py
+++ test_script/mesh.py
@@ -176,6 +176,39 @@ class mesh:
         self.n_face = len(faces)
         self.n_edge = self.edges.shape[0]
         return
+    
+    def read_mesh_file(self,filename='output.mesh'):
+        n_line = 0
+        i_v = 0
+        i_e = 0
+        i_f = 0
+        with open(filename,'r') as f:
+            n_line = 0
+            for line in f:
+                p = list(filter(lambda x: x != '',line.strip().split(' ')))
+                if n_line == 0:
+                    self.n_vertex = int(p[0])
+                    self.n_edge = int(p[1])
+                    self.n_face = int(p[2])
+                    self.vertices = np.zeros((self.n_vertex,3),dtype=float)
+                    self.faces = np.zeros((self.n_face,3),dtype=int)
+                    self.edges = np.zeros((self.n_edge,3),dtype=int)
+                elif n_line < self.n_vertex + 1:
+                    if i_v < self.n_vertex:
+                        for i in range(3):
+                            self.vertices[i_v][i] = float(p[i])
+                        i_v += 1
+                elif n_line < self.n_vertex + self.n_edge + 1:
+                    if i_e < self.n_edge:
+                        for i in range(2):
+                            self.edges[i_e][i] = int(p[i])
+                        i_e += 1
+                else:
+                    if i_f < self.n_face:
+                        for i in range(int(p[0])):
+                            self.faces[i_f][i] = int(p[i+1])
+                n_line += 1
+        return
 
     def save_edges_file(self,filename):
         np.savetxt(filename,self.edges,fmt=['%d','%d'])
