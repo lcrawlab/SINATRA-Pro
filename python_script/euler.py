9b6b5e563af23d87161030b375ef1f87af9c05d3
wtang8 <wai_shing_tang@brown.edu>
2020-12-21 18:02:11 -0500

    Wai Shing 12/21/2020

--- python_script/euler.py
+++ python_script/euler.py
@@ -2,49 +2,56 @@
 
 from mesh import *
 from directions import *
+import os, sys
 import numpy as np
-import multiprocessing
-from joblib import Parallel, delayed
+from fast_histogram import histogram1d
 
-#' Computes the EC curve on a ball
-#'
-#' @export
-#' @description Computes the EC curve of a mesh on a bounding ball of specified radius $radius$. The filtration steps for which the
-#' (S/D) EC curve are computed is done relative to the bounding ball. For filtrations steps that are computed relative to the shape, use
-#' \code{compute_discrete_ec_curve}. For comparisons with multiple shapes, this is the recommended function.
-#'
-#' @param complex (list) : The list containing metadata about the mesh
-#' @param directions (array)
-#' @param n_filtration (int): The number of sub-level sets for which to compute the EC curve on in a given direction.
-#' @param ball_radius (float): The radius of the bounding ball.
-#'
-#' @return radius
-#' @return ec_curve (nxp matrix): A matrix containing the EC curve of the given simplicial complex with the first index as the projections for which the EC was computed on.
-
-def compute_ec_curve_single(mesh, direction, radius, n_filtration = 25, ec_type = "ECT", include_faces = True):
+## Computes the Euler Characteristics (EC) curves in a given direction for a given mesh
+##
+## Description: Computes the EC curve of a mesh along a given direction in discrete filtration steps.
+##
+## Parameters
+## mesh (class mesh) : The mesh class in mesh.py containing vertices, edges, and faces of the mesh
+## direction
+## @param n_filtration (int): The number of sub-level sets for which to compute the EC curve on in a given direction.
+## @param ball_radius (float): The radius of the bounding ball.
+##
+## @return radius
+## @return ec_curve (nxp matrix): A matrix containing the EC curve of the given simplicial complex with the first index as the projections for which the EC was computed on.
+def compute_ec_curve_single(mesh, direction, ball_radius, n_filtration = 25, ec_type = "ECT", include_faces = True):
+    
     eulers = np.zeros(n_filtration,dtype=float)
+    
     vertex_function = np.dot(mesh.vertices,direction) 
+    radius = np.linspace(-ball_radius,ball_radius,n_filtration)
+   
     # filtrating vertices
-    vertex_hist, bin_edges = np.histogram(vertex_function,bins=radius)
-    V = np.cumsum(vertex_hist)
+    V = histogram1d(vertex_function,range=[-ball_radius,ball_radius],bins=(n_filtration-1))
+    
     # filtrating edges
     edge_function = np.amax(vertex_function[mesh.edges],axis=1)
-    edge_hist, bin_edges = np.histogram(edge_function,bins=radius)
-    E = np.cumsum(edge_hist)
+    E = histogram1d(edge_function,range=[-ball_radius,ball_radius],bins=(n_filtration-1))
+    
     if include_faces:
         # filtrating faces
-        face_function = [np.amax(vertex_function[face]) for face in mesh.faces]
-        face_hist, bin_edges = np.histogram(face_function,bins=radius)
-        F = np.cumsum(face_hist)
+        try:
+            face_function = np.amax(vertex_function[mesh.faces],axis=1)
+        except:
+            face_function = [np.amax(vertex_function[face]) for face in mesh.faces]
+        F = histogram1d(face_function,range=[-ball_radius,ball_radius],bins=(n_filtration-1))
     else:
         F = 0
+        
     eulers[1:] = V - E + F
+
     if ec_type == "ECT":
+        eulers = np.cumsum(eulers)
         return eulers
     elif ec_type == "DECT":
-        eulers[1:] = (eulers[1:]-eulers[:-1])/(radius[1:]-radius[:-1])
+        eulers[1:] = eulers[1:] / (radius[1:]-radius[:-1])
         return eulers
     elif ec_type == "SECT":
+        eulers = np.cumsum(eulers)
         eulers -= np.mean(eulers[i])
         eulers = np.cumsum(eulers)*((radius[-1]-radius[0])/n_filtration)
         return eulers
@@ -53,24 +60,26 @@ def compute_ec_curve_single(mesh, direction, radius, n_filtration = 25, ec_type 
 
 def compute_ec_curve(mesh, directions, n_filtration = 25, ball_radius = 1.0, ec_type = "ECT", first_column_index = False, include_faces = True):
     eulers = np.zeros((directions.shape[0],n_filtration),dtype=float)
-    radius = np.linspace(-ball_radius,ball_radius,n_filtration)    
     for i in range(directions.shape[0]):
-        eulers[i] = compute_ec_curve_single(mesh,directions[i],radius,n_filtration,ec_type,include_faces)
+        eulers[i] = compute_ec_curve_single(mesh,directions[i],ball_radius,n_filtration,ec_type,include_faces)
+    radius = np.linspace(-ball_radius,ball_radius,n_filtration)
     return radius, eulers
 
 def compute_ec_curve_parallel(mesh, directions, n_filtration = 25, ball_radius = 1.0, ec_type = "ECT", include_faces = True, n_core = -1):
-    radius = np.linspace(-ball_radius,ball_radius,n_filtration)
-    parameter = (n_filtration,ball_radius,ec_type,include_faces)
+    import multiprocessing
+    from joblib import Parallel, delayed
+    parameter = (ball_radius,n_filtration,ec_type,include_faces)
     if n_core == -1:    
         n_core = multiprocessing.cpu_count()
-    processed_list = Parallel(n_jobs=n_core)(delayed(compute_ec_curve_single)(mesh,direction,radius,*parameter) for direction in directions)
+    processed_list = Parallel(n_jobs=n_core)(delayed(compute_ec_curve_single)(mesh,direction,*parameter) for direction in directions)
     processed_list = np.array(processed_list)
+    radius = np.linspace(-ball_radius,ball_radius,n_filtration)
     return radius, processed_list
 
-def compute_ec_curve_folder(protA, protB, directions, n_sample = 101, ec_type = "ECT", n_cone = 15, directions_per_cone = 4, cap_radius = 0.1, n_filtration = 25, ball_radius = 1.0, include_faces = True, directory_mesh_A = None, directory_mesh_B = None, sm_radius = 4.0, hemisphere=False, parallel = False, n_core = -1):
+def compute_ec_curve_folder(protA, protB, directions, n_sample = 101, ec_type = "ECT", n_cone = 15, n_direction_per_cone = 4, cap_radius = 0.1, n_filtration = 25, ball_radius = 1.0, include_faces = True, directory_mesh_A = None, directory_mesh_B = None, sm_radius = 4.0, hemisphere=False, parallel = False, n_core = -1):
 
     if directory_mesh_A == None or directory_mesh_B == None:
-        directory = "%s_%s"
+        directory = "%s_%s"%(protA,protB)
         directory_mesh_A = "%s_%s/mesh/%s_%.1f"%(protA,protB,protA,sm_radius)
         directory_mesh_B = "%s_%s/mesh/%s_%.1f"%(protA,protB,protB,sm_radius) 
         if not os.path.exists(directory):
@@ -95,9 +104,9 @@ def compute_ec_curve_folder(protA, protB, directions, n_sample = 101, ec_type = 
                 ecs.append(ec.flatten())
             ecs = np.array(ecs)
             if include_faces:
-                outfile = '%s/%s_%s_%d_%d_%.1f_%d_%d.txt'%(directory,ec_type,prot,n_cone,directions_per_cone,cap_radius,n_filtration)
+                outfile = '%s/%s_%s_%d_%d_%.1f_%d.txt'%(directory,ec_type,prot,n_cone,n_direction_per_cone,cap_radius,n_filtration)
             else:
-                outfile = '%s/%s_%s_%d_%d_%.1f_%d_%d_nofaces.txt'%(directory,ec_type,prot,n_cone,directions_per_cone,cap_radius,n_filtration)
+                outfile = '%s/%s_%s_%d_%d_%.1f_%d_nofaces.txt'%(directory,ec_type,prot,n_cone,n_direction_per_cone,cap_radius,n_filtration)
             
             np.savetxt(outfile,ecs,fmt='%.3f')
             outfiles.append(outfile)
@@ -120,9 +129,9 @@ def compute_ec_curve_folder(protA, protB, directions, n_sample = 101, ec_type = 
                     ecs.append(ec.flatten())
             ecs = np.array(ecs)
             if include_faces:
-                outfile = '%s_%s_%d_%d_%.1f_%d_%d.txt'%(ec_type,prot,n_cone,directions_per_cone,cap_radius,n_filtration)
+                outfile = '%s_%s_%d_%d_%.1f_%d_%d.txt'%(ec_type,prot,n_cone,n_direction_per_cone,cap_radius,n_filtration)
             else:
-                outfile = '%s_%s_%d_%d_%.1f_%d_%d_nofaces.txt'%(ec_type,prot,n_cone,directions_per_cone,cap_radius,n_filtration)
+                outfile = '%s_%s_%d_%d_%.1f_%d_%d_nofaces.txt'%(ec_type,prot,n_cone,n_direction_per_cone,cap_radius,n_filtration)
 
             np.savetxt(outfile,ecs,fmt='%.3f')
             outfiles.append(outfile)
@@ -145,8 +154,8 @@ def compute_ec_curve_folder(protA, protB, directions, n_sample = 101, ec_type = 
     data = np.subtract(data,mean)
     data = np.divide(data,std)
     
-    normec_file = '%s/%s_%s_%s_%d_%d_%.1f_%d_%d_norm.txt'%(directory,ec_type,protA,protB,n_cone,directions_per_cone,cap_radius,n_filtration)
-    notvacuum_file = '%s/notvacuum_%s_%s_%s_%d_%d_%.1f_%d_%d_norm.txt'%(directory,ec_type,protA,protB,n_cone,directions_per_cone,cap_radius,n_filtration)
+    normec_file = '%s/%s_%s_%s_%d_%d_%.1f_%d_norm.txt'%(directory,ec_type,protA,protB,n_cone,n_direction_per_cone,cap_radius,n_filtration)
+    notvacuum_file = '%s/notvacuum_%s_%s_%s_%d_%d_%.1f_%d_norm.txt'%(directory,ec_type,protA,protB,n_cone,n_direction_per_cone,cap_radius,n_filtration)
     np.savetxt(normec_file,data)
     np.savetxt(notvacuum_file,not_vacuum,fmt="%d")
      
@@ -155,6 +164,7 @@ def compute_ec_curve_folder(protA, protB, directions, n_sample = 101, ec_type = 
     label = np.zeros(n_A+n_B,dtype=int)
     label[:n_A].fill(-1)
     label[n_A:].fill(1)
+    np.savetxt('%s/%s_%s_label.txt'%(directory,protA,protB),label)
 
     return data, label, not_vacuum
 
---
+++ python_script/main_SINATRA.py
@@ -0,0 +1,66 @@
+#!/bin/python3
+
+from traj_reader import *
+from euler import *
+from gp import *
+from reconstruction import *
+
+protA = "WT"
+protB = "R164S"
+nsample = 101
+
+## Input files
+struct_file_A = 'data/%s/md_0_1.gro'%protA
+traj_file_A = 'data/%s/md_0_1_noPBC.xtc'%protA
+
+struct_file_B = 'data/%s/md_0_1.gro'%protB
+traj_file_B = 'data/%s/md_0_1_noPBC.xtc'%protB
+
+## Simplicies construction parameters
+selection = 'resid 65:213'
+sm_radius = 4.0
+
+## Filtration directions parameters
+n_cone = 15
+n_direction_per_cone = 4
+cap_radius = 0.1
+
+## Euler Characteristics (EC) calculation parameters
+ec_type = "DECT"
+n_filtration = 25
+n_sample = 101
+parallel = False ## using multiple CPU cores for calculation
+n_core = 4 ## Number of cores used for EC calculation
+
+## Variable selection parameters
+bandwidth = 0.01
+sampling_method = "ESS"
+
+## Read trajectory file and output aligned protein structures in pdb format
+convert_traj_pdb_aligned(protA, protB, struct_file_A = struct_file_A, traj_file_A = traj_file_A, struct_file_B = struct_file_B, traj_file_B = traj_file_B, nsample = 101,selection=selection)
+
+## Converted protein structures into simplicial mesehes
+convert_pdb_mesh(protA,protB, n_sample = n_sample, radius = sm_radius)
+
+## Calculate distributed cones of directions for EC calculations
+directions = generate_equidistributed_cones(n_cone=n_cone,n_direction_per_cone=n_direction_per_cone,cap_radius=cap_radius,hemisphere=False)
+#np.savetxt("WT_R164S/directions_4_15_0.1.txt",directions)
+
+## EC calculations to convert simplicial meshes to topological summary statistics
+X, y, not_vacuum = compute_ec_curve_folder(protA,protB,directions,n_sample=n_sample,ec_type=ec_type,n_cone=n_cone,n_direction_per_cone=n_direction_per_cone,cap_radius=cap_radius,n_filtration=n_filtration,sm_radius=sm_radius,parallel=parallel,n_core=n_core)
+#X = np.loadtxt("WT_R164S/DECT_WT_R164S_15_4_0.1_25_norm.txt")
+#not_vacuum = np.loadtxt("WT_R164S/notvacuum_DECT_WT_R164S_15_4_0.1_25_norm.txt")
+#y = np.loadtxt('WT_R164S/WT_R164S_label.txt')
+
+## RATE calculation for variable selections from the topological summary statistics
+kld, rates, delta, eff_samp_size = find_rate_variables_with_other_sampling_methods(X,y,bandwidth=bandwidth,sampling_method=sampling_method)
+np.savetxt("WT_R164S/rates_DECT_WT_R164S_15_4_0.1_25.txt",rates)
+
+## reconstruct the RATE values onto the protein structures for visualization
+## reconstruct probabilities are stored in "Temperature factor" column in the pdb format
+## can then be visualized using Chimera or pymol 
+#rates = np.loadtxt("WT_R164S/rates_DECT_WT_R164S_15_4_0.1_25.txt")
+#directions = np.loadtxt("WT_R164S/directions_4_15_0.1.txt")
+vert_prob = reconstruct_on_multiple_mesh(protA,protB,directions,rates,not_vacuum,n_sample=n_sample,n_direction_per_cone=4,n_filtration=25,sm_radius=sm_radius)
+write_vert_prob_on_pdb(vert_prob,protA=protA,protB=protB,selection=selection)
+
