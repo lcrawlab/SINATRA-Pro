b84eabb6c519919d91d961df3a89ef8035f5012b
wtang8 <wai_shing_tang@brown.edu>
2021-05-04 05:34:33 -0400

    5/4/2021 Wai Shing added Cpp script

--- sinatra_cpp/src/main.cpp
+++ sinatra_cpp/src/main.cpp
@@ -77,10 +77,10 @@ int main ()
     }
     cout << "Mesh Construction Complete.      \n";
 
-    vector<vector<double> > directions = generate_equidistributed_cones(5,0.8,4,false);
+    vector<vector<double> > directions = generate_equidistributed_cones(10,0.8,4,false);
     cout << directions.size() << " Directions generated\n";
     ofstream ecfile, yfile;
-    ecfile.open("DECT_5_4_0.8_50.txt");
+    ecfile.open("DECT_10_4_0.8_60.txt");
     yfile.open("label_WT_R164S.txt");
     //vector<vector<double> > ec_matrix;
     //vector<int> label;
@@ -89,7 +89,7 @@ int main ()
         mesh meshA;
         string mshfile = mshpath_A + "WT_frame" + to_string(i_frame) + ".msh";
         meshA.read_mesh(mshfile);
-        vector<vector<double> > ec_curves = compute_ec_curve(meshA,directions,1.0,50,"DECT");
+        vector<vector<double> > ec_curves = compute_ec_curve(meshA,directions,1.0,60,"DECT");
         //vector<double> flattened(begin(ec_curves[0]), end(ec_curves[0]));
         //for(int i_dir=1;i_dir<ec_curves.size();i_dir++)
         //    flattened.insert(end(flattened), begin(ec_curves[i_dir]), end(ec_curves[i_dir]));
@@ -110,7 +110,7 @@ int main ()
         mesh meshA;
         string mshfile = mshpath_B + "R164S_frame" + to_string(i_frame) + ".msh";
         meshA.read_mesh(mshfile);
-        vector<vector<double> > ec_curves = compute_ec_curve(meshA,directions,1.0,50,"DECT");
+        vector<vector<double> > ec_curves = compute_ec_curve(meshA,directions,1.0,60,"DECT");
         for(int i_dir=0;i_dir<ec_curves.size();i_dir++)
         {
             for(int i_fil=0;i_fil<ec_curves[i_dir].size();i_fil++)
@@ -127,26 +127,26 @@ int main ()
         //label.push_back(1);
     }
     cout << "EC Calculation Complete.      \n";
-
+    /*
     dmat X;
     dvec y;
-    X.load("DECT_5_4_0.8_50.txt",raw_ascii);
+    X.load("DECT_10_4_0.8_60.txt",raw_ascii);
     y.load("label_WT_R164S.txt",raw_ascii);
     dvec rates = find_rate_variables_with_other_sampling_methods(X,y);
-    rates.save("rates_5_4_0.8_50.txt",raw_ascii);
+    rates.save("rates_10_4_0.8_60.txt",raw_ascii);
     
-    vector<vector<double> > directions = generate_equidistributed_cones(5,0.8,4,false);
+    vector<vector<double> > directions = generate_equidistributed_cones(10,0.8,4,false);
     dvec rates_dvec;
-    rates_dvec.load("rates_5_4_0.8_50.txt",raw_ascii);
+    rates_dvec.load("rates_10_4_0.8_60.txt",raw_ascii);
     string mshpath_A = "../msh/WT_offset_0/";
     string mshfile = mshpath_A + "WT_frame0.msh";
     vector<double> rates_vert = reconstruct_by_sorted_threshold(mshfile,directions,rates_dvec,50,4);
     ofstream rfile;
-    rfile.open("rates_vert.txt");
+    rfile.open("rates_vert_10_4_0.8_60.txt");
     for(int i=0;i<rates_vert.size();i++)
         rfile << rates_vert[i] << '\n';
     rfile.close();
-    
+    */
     return 0;
 }
 
--- sinatra_cpp/src/test_gp.cpp
+++ sinatra_cpp/src/test_gp.cpp
@@ -1,4 +1,5 @@
 #include <armadillo>
+#include "tools.h"
 #include "gp.h"
 #include <omp.h>
 
@@ -8,11 +9,10 @@ int main()
     omp_set_num_threads(number_of_threads);
     dmat X;
     dvec y;
-    //X.load("../output/DECT_20_8_0.8_100.txt",raw_ascii);
-    X.load("DECT_5_4_0.8_50.txt",raw_ascii);
+    X.load("DECT_10_4_0.8_60.txt",raw_ascii);
     y.load("label_WT_R164S.txt",raw_ascii);
     dvec rates = find_rate_variables_with_other_sampling_methods(X,y);
-    rates.save("rates_5_4_0.8_50.txt",raw_ascii);
+    rates.save("rates_10_4_0.8_60.txt",raw_ascii);
     return 0;
 }
 
