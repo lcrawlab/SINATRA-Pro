58eed95a1351010a064e2695526493d115943615
wtang8 <wai_shing_tang@brown.edu>
2021-06-30 03:06:44 -0400

    6/30/2021 Wai Shing

--- python_script/simplices_construction.py
+++ python_script/simplices_construction.py
@@ -129,66 +129,7 @@ class ComplexFiltration:
             for face in self.faces:
                 f.write('%d  %d %d %d  \n'%(len(face),face[0],face[1],face[2]))
         return
-    
-    """
-    def plot_EC_curve(self,radii=np.linspace(0,1,21),outfile='ec_filtration.pdf'):
-        ECs = []
-        for radius in radii:
-            edges, distances = self.get_edge_list(radius=radius)
-            faces = self.edge_to_face_list(edges=edges)
-            V = self.n_vertices
-            E = edges.shape[0]
-            F = faces.shape[0]
-            EC = V - E + F
-            ECs.append(EC)
-            print('%.1f %d %d %d %d'%(radius,V,E,F,EC))
 
-        fig, ax = plt.subplots()
-        ax.plot(radii,ECs,'ko-')
-        ax.set_xlim(radii[0],radii[-1])
-        ax.set_ylim(0,1000)
-        ax.set_xlabel('radius',fontsize=20)
-        ax.set_ylabel('Euler characteristics',fontsize=20)
-        ax.tick_params(axis='both',labelsize=20)
-        plt.tight_layout()
-        plt.savefig(outfile)
-        plt.show()
-        plt.close()
-        return
-    
-    def plot_faces_2D(self,edges,faces,radius=1.0,filename='output.pdf'):
-
-        fig, ax = plt.subplots()
-
-        vertices = self.vertices[:,:2]
-        ax.scatter(vertices[:,0],vertices[:,1],color='k')
-        for edge in edges:
-            x = [vertices[edge[0],0],vertices[edge[1],0]]
-            y = [vertices[edge[0],1],vertices[edge[1],1]]
-            ax.plot(x,y,'k')
-
-        patches = []
-        for face in faces:
-            polygon = Polygon([vertices[face[0]],vertices[face[1]],vertices[face[2]]],True)
-            patches.append(polygon)
-
-        p = PatchCollection(patches, cmap=matplotlib.cm.jet, alpha=0.4)
-        colors = 100*np.random.rand(len(patches))
-        p.set_array(np.array(colors))
-        ax.add_collection(p)
-
-        ax.set_xlim(-radius,radius)
-        ax.set_ylim(-radius,radius)
-        ax.set_xlabel(r'$x$',fontsize=20)
-        ax.set_ylabel(r'$y$',fontsize=20)
-        ax.tick_params(axis='both',labelsize=20)
-        plt.tight_layout()
-        plt.savefig(filename)
-        plt.show()
-        plt.close()
-        return
-    """
-    
     def normalize(self):
         rmax = np.amax(np.linalg.norm(self.vertices,axis=1))
         self.vertices /= rmax
