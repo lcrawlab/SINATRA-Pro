8afd369beb912581495339dc12ae43e9af0b5a68
wtang8 <wai_shing_tang@brown.edu>
2021-07-13 22:18:12 -0400

    Python package 7/13/2021 Wai Shing

--- python_package/tests/test.sh
+++ python_package/tests/test.sh
@@ -1,12 +1,12 @@
 #!/bin/python3
 
-python3 ../src/main.py --protA WT --protB R164S \
+python3 ../src/sinatra_pro/__main__.py --protA WT --protB R164S \
                 --directory "WT_R164S_65_230_2.0" \
                 --n_sample 10 \
-                --struct_file_A "../data/wt.gro" \
-                --traj_file_A "../data/wt.xtc" \
-                --struct_file_B "../data/r164s.gro" \
-                --traj_file_B "../data/r164s.xtc" \
+                --struct_file_A "../../python_script/data/WT/md_0_1.gro" \
+                --traj_file_A "../../python_script/data/WT/md_0_1_noPBC.xtc" \
+                --struct_file_B "../python_script/data/R164S/md_0_1.gro" \
+                --traj_file_B "../../python_script/data/R164S/md_0_1_noPBC.xtc" \
                 --selection "protein and resid 65:230" \
                 --radius 2.0 \
                 --n_cone 1 \
@@ -14,5 +14,6 @@ python3 ../src/main.py --protA WT --protB R164S \
                 --cap_radius 0.80 \
                 --ec_type "DECT" \
                 --n_filtration 20 \
-                --n_mcmc 10000 \
-                --parallel --n_core 4 --verbose
+                --n_mcmc 100000 \
+                --parallel \
+                --n_core 4 --verbose
