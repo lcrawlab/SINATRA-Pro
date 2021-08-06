ad408c326d9c2c5e0934b16e801ed2308ea72aa7
wtang8 <wai_shing_tang@brown.edu>
2021-08-06 05:09:58 -0400

    updated main.cpp

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -14,62 +14,98 @@
 #include "gp.h"
 #include "reconstruction.h"
 
+#include <thread>
+
 using namespace std;
 using namespace arma;
 
 int main (int argc, char *argv[])
 {
     
-    int number_of_threads = 4;
-    omp_set_num_threads(number_of_threads);
-    
     string ec_type = "DECT";
-    int n_filtration = 20;
-    int n_cone = 1;
-    int n_direction_per_cone = 1;
+    int n_filtration = 60;
+    int n_cone = 4;
+    int n_direction_per_cone = 4;
     double cap_radius = 0.80;
-    double cutoff = 2.0;
-    
-    string directory = "../WT_R164S_65_230";
-    //string pdbpath_A = "../../python_script/WT_R164S_65_230/pdb/WT_offset_0/";
-    string pdbpath_A = "/mnt/d/share/sinatra_python/python_package/tests/WT_R164S_65_230_2.0/pdb/WT_offset_0/";
-    string mshpath_A = "../WT_R164S_65_230/msh/WT_offset_0/";
-    //string pdbpath_B = "../../python_script/WT_R164S_65_230/pdb/R164S_offset_0/";
-    string pdbpath_B = "/mnt/d/share/sinatra_python/python_package/tests/WT_R164S_65_230_2.0/pdb/R164S_offset_0/";
-    string mshpath_B = "../WT_R164S_65_230/msh/R164S_offset_0/";
-    /** 
-    
-      cout << "Path to PDB files for protein A: ";
-    cin >> pdbpath_A;
-    cout << "Path to PDB files for protein B: ";
-    cin >> pdbpath_B;
-    cout << "Path to MSH files for protein A: ";
-    cin >> mshpath_A;
-    cout << "Path to MSH files for protein B: ";
-    cin >> mshpath_B;
-    **/
-    
-    string pdbfile_recon = pdbpath_A + "WT_frame0.pdb";
+    double cutoff = 4.0;
     string y_filename = "label.txt"; 
+   
+    string prot_A = "protA";
+    string prot_B = "protB";
+    string directory = "output";
+    string pdbpath_A = "data/pdb/WT_offset_0/";
+    string pdbpath_B = "data/pdb/R164S_offset_0/";
+    string pdbfile_recon = pdbpath_A + "WT_frame0.pdb";
+    
+    string input;
+    cout << "Name for protein A: ";
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> prot_A;
+    }
+    cout << "Name for protein B: ";
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> prot_B;
+    }
+    cout << "Path to PDB files for protein A: ";
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> pdbpath_A;
+    }
+    cout << "Path to PDB files for protein B: ";
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> pdbpath_B;
+    }
+    cout << "PDB file for visualization protein A: ";
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> pdbfile_recon;
+    }
+    cout << "Directory for output files (default: output): ";
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> directory;
+    }
 
-    /**
+    string mshpath_A = directory + "msh/" + prot_A + "/";
+    string mshpath_B = directory + "msh/" + prot_B + "/";
+   
     string tmp;
     cout << "Type of Euler characteristic measure (DECT/ECT/SECT, default: DECT): ";
-    cin >> tmp;
+    getline(cin,input);
+    if ( !input.empty() ) {
+        istringstream stream(input);
+        stream >> tmp;
+    }
     if (tmp.compare("SECT") == 0) { ec_type = "SECT"; }
     else if (tmp.compare("ECT") == 0) { ec_type = "ECT"; }
     else {ec_type = "DECT"; }
-    cout << "Radius cutoff for simplicial construction: ";
+
+    cout << "Radius cutoff for simplicial construction (default: 4.0): ";
     cin >> cutoff;
-    cout << "Number of cones: ";
+    cout << "Number of cones: (default: 4)";
     cin >> n_cone;
-    cout << "Number of directions per cone: ";
+    cout << "Number of directions per cone (default: 4): ";
     cin >> n_direction_per_cone;
     cout << "Cap radius: ";
     cin >> cap_radius;
-    cout << "Number of filtration steps: ";
+    cout << "Number of filtration steps (default: 60): ";
     cin >> n_filtration;
-    **/
+    
+    int number_of_threads = -1;
+    cout << "Number of CPU cores used for calculation (default: -1, which automatically detect all cores): ";
+    cin >> number_of_threads;
+    if (number_of_threads == -1)
+        const auto processor_count = thread::hardware_concurrency();    //may return 0 when not able to detect
+    omp_set_num_threads(number_of_threads); 
 
     string filesuffix;
     stringstream ss_filesuffix;
@@ -79,7 +115,6 @@ int main (int argc, char *argv[])
     pdb_to_mesh(pdbpath_A,pdbpath_B,mshpath_A,mshpath_B,cutoff);
     vector<vector<double> > directions = generate_equidistributed_cones(n_cone,cap_radius,n_direction_per_cone,false);
     cout << directions.size() << " Directions generated\n";
-    cout << directions[0][0] << ' ' << directions[0][1] << ' ' << directions[0][2] << endl;
     
     string ec_filename = directory + "/" + ec_type + filesuffix + ".txt";
     compute_ec_curve_multiple_files(mshpath_A,mshpath_B,directions,n_filtration,ec_type,y_filename,ec_filename);  
@@ -98,7 +133,6 @@ int main (int argc, char *argv[])
     
     dvec rates_vert = conv_to<dvec>::from(vec_rates_vert);
     rates_vert.save(directory+"/rates_vert"+filesuffix+".txt",raw_ascii); 
-    //rates_vert.load(directory+"/rates_vert"+filesuffix+".txt",raw_ascii);
     add_rate_pdb(pdbfile_recon,directory+"/rates_vert"+filesuffix+".pdb",rates_vert);
     
     return 0;
